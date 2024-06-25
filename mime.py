c728ac9fec69aa67d94ab12e7277854e56cede51
Osman Çalışkan <95987598+osmnclskn@users.noreply.github.com>
2024-06-25 14:22:50 +0300

    Update mime.py

--- mime.py
+++ mime.py
@@ -1,12 +1,14 @@
-import streamlit as st
-import openai
+import json
+import os
+
 import magic
+import openai
+import streamlit as st
+from dotenv import load_dotenv
 from langchain.document_loaders.parsers import BS4HTMLParser, PDFMinerParser
 from langchain.document_loaders.parsers.txt import TextParser
 from langchain_community.document_loaders import Blob
-from dotenv import load_dotenv
-import os
-import json
+
 from questiongenerate import PQMQualityControlQuestionGenerator
 
 load_dotenv()
@@ -20,49 +22,60 @@ HANDLERS = {
     "text/html": BS4HTMLParser(),
 }
 
+
+def get_mime_type(data):
+    mime = magic.Magic(mime=True)
+    return mime.from_buffer(data)
+
+
+def process_uploaded_file(data):
+    mime_type = get_mime_type(data)
+    print(f"Mime Type :{mime_type}")
+
+    blob = Blob.from_data(data=data, mime_type=mime_type)
+    print(f"Blob created with MIME type {mime_type}")
+    parser = HANDLERS.get(mime_type)
+    if parser:
+        documents = parser.parse(blob=blob)
+        print(f"Processed Data : {documents}")
+        return "".join([doc.page_content for doc in documents])
+    else:
+        st.error(f"No parser found for MIME Type : {mime_type}")
+        return None
+
+
+def generate_questions(documents_content):
+    pqm_question_generator = PQMQualityControlQuestionGenerator()
+    questions = []
+    for i in range(10):
+        structured_out = pqm_question_generator.generate_question(
+            documents_content, questions
+        )
+        questions.append(structured_out)
+    return questions
+
+
+def display_questions(questions):
+    for idx, question in enumerate(questions):
+        st.write(f"{idx+1}.Generated Questions Output:")
+        st.json(json.dumps(question.__dict__, indent=2))
+
+
 def main():
     st.title("PQM Quality Control Question Generator")
-
-    uploaded_file = st.file_uploader('Upload a file', type=["pdf", "txt", "html"])
-    
+    uploaded_file = st.file_uploader("Upload a file", type=["pdf", "txt", "html"])
     if uploaded_file is not None:
-        st.success("File uploaded successfully!")
+        st.success("File uploaded succesfully!")
         data = uploaded_file.read()
 
-        # MIME türünün belirlenmesi
-        mime = magic.Magic(mime=True)
-        mime_type = mime.from_buffer(data)
+        mime_type = get_mime_type(data)
         print(f"MIME Type: {mime_type}")
+        documents_content = process_uploaded_file(data)
 
-        # Blob oluşturma
-        blob = Blob.from_data(
-            data=data,
-            mime_type=mime_type,
-        )
-        print(f"Blob created with MIME type {mime_type}")
-        
-        parser = HANDLERS.get(mime_type)
-        if parser:
-            documents = parser.parse(blob=blob)
-            print(f"Processed Data: {documents}")
+        if documents_content:
+            questions = generate_questions(documents_content)
+            display_questions(questions)
 
-            # Documents içeriğini stringe çevirme
-            documents_content = " ".join([doc.page_content for doc in documents])
-        
-            pqm_question_generator = PQMQualityControlQuestionGenerator()
 
-            # OpenAI ile soru üretme
-            questions=[]
-            for i in range(10):
-                structured_out = pqm_question_generator.generate_question(documents_content, questions)
-                questions.append(structured_out)
-                
-
-            for idx in range(len(questions)):
-                st.write(f"{idx+1}. Generated Question Output: ")
-                st.json(json.dumps(questions[idx].__dict__, indent=2))
-        else:
-            st.error(f"No parser found for MIME Type: {mime_type}")
-        
 if __name__ == "__main__":
     main()
